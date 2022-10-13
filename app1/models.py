e35ede6d47757845c6fdc2c142e8caf736533416
Luciano <maximo.boga@gmail.com>
2022-10-13 18:23:26 -0300

    detalle de los models

--- app1/models.py
+++ app1/models.py
@@ -3,6 +3,8 @@ from django.db import models
 # Create your models here.
 
 class Vuelo(models.Model):
+    def __str__(self):
+        return f'Vuelo: {self.id_vuelo} sale el: {self.fecha}'
     id_vuelo = models.IntegerField()
     salida = models.CharField(max_length=50)
     destino = models.CharField(max_length=50)
@@ -10,12 +12,16 @@ class Vuelo(models.Model):
     hora_de_salida = models.TimeField() #02:13(HH:MM)
 
 class Personal(models.Model):
+    def __str__(self):
+        return f'Personal: {self.profesion} del vuelo: {self.id_vuelo}'
     nombre = models.CharField(max_length=50)
     apellido = models.CharField(max_length=50)
     profesion = models.CharField(max_length=50)
     id_vuelo = models.IntegerField()
 
 class Pasajero(models.Model):
+    def __str__(self):
+        return f'Pasajero: {self.nombre} {self.apellido} DNI:{self.documento}' 
     nombre = models.CharField(max_length=50)
     apellido = models.CharField(max_length=50)
     documento = models.IntegerField()
